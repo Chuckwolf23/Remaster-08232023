b4dc899f0f64d24e81d693b2b2cb92f66133af80
Chuckwolf23 <championc17@icloud.com>
2023-08-25 19:09:17 -0500

    Done

--- problems/09-count-scores.js
+++ problems/09-count-scores.js
@@ -27,9 +27,32 @@ let peeps = [
 console.log(countScores(peeps)); //=> { Anthony: 4, Fred: 4, Winnie: 6 }
 ***********************************************************************/
 
-function countScores(people) {
-  // Your code here
-}
+function countScores(people) {//Write a function `countScores(people)` that takes in an array of score objects (people)
+  let obj = {}// placement for all scores
+  for( let info of people){// loop through the object and pulls both keys and values
 
+    if(obj[info.name]===undefined){ //OBJ[] = GRABS KEYS/ OBJ.WORD = GRABS VALUES
+      obj[info.name] = info.score
+    } else
+    { obj[info.name]  += info.score }// if the persone name is in there already then add the score
+  }
+  return obj
+}
+let ppl = [{name: "Anthony", score: 10},
+            {name: "Fred", score : 10},
+            {name: "Anthony", score: -8},
+            {name: "Winnie", score: 12}];
+
+console.log(countScores(ppl)); //=> { Anthony: 2, Fred: 10, Winnie: 12 }
+let peeps = [
+  {name: "Anthony", score: 2},
+  {name: "Winnie", score: 2},
+  {name: "Fred", score: 2},
+  {name: "Winnie", score: 2},
+  {name: "Fred", score: 2},
+  {name: "Anthony", score: 2},
+  {name: "Winnie", score: 2}
+];
+console.log(countScores(peeps)); //=> { Anthony: 4, Fred: 4, Winnie: 6 }
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = countScores;
